646e160fa3282d330de4c7cc98977f91f45964ed
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2023-10-20 16:52:43 +0100

    Add API Gateway
    Use OpenAPI schema to define REST API, then add logging and IAM execution permissions.
    
    Add an account-level IAM role for logging to CloudWatch.
    
    Remove lambda function URLs. Test against REST API base URL.
    
    Fixes VEGA-2014 #minor

---
+++ terraform/account/terraform.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "s3" {
+    bucket         = "opg.terraform.state"
+    key            = "opg-data-lpa-deed-account/terraform.tfstate"
+    encrypt        = true
+    region         = "eu-west-1"
+    role_arn       = "arn:aws:iam::311462405659:role/lpa-store-ci"
+    dynamodb_table = "remote_lock"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.8.0"
+    }
+  }
+  required_version = ">= 1.4.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::${local.account.account_id}:role/${var.default_role}"
+    session_name = "terraform-session"
+  }
+
+  default_tags {
+    tags = local.default_tags
+  }
+}
