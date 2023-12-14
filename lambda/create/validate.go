a564a5fe1305a37992a9cc98a74374e1a79d766e
Joshua Hawxwell <m@hawx.me>
2023-12-14 11:50:35 +0000

    Add more data to the LPA

--- lambda/create/validate.go
+++ lambda/create/validate.go
@@ -3,22 +3,124 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"time"
 
 	"github.com/ministryofjustice/opg-data-lpa-store/internal/shared"
 )
 
-func validateAddress(address shared.Address, prefix string, errors []shared.FieldError) []shared.FieldError {
-	requiredFields := map[string]string{
-		fmt.Sprintf("%s/line1", prefix):   address.Line1,
-		fmt.Sprintf("%s/town", prefix):    address.Town,
-		fmt.Sprintf("%s/country", prefix): address.Country,
-	}
+func Validate(lpa shared.LpaInit) []shared.FieldError {
+	activeAttorneyCount, replacementAttorneyCount := countAttorneys(lpa.Attorneys)
 
-	for source, value := range requiredFields {
-		if value == "" {
-			errors = append(errors, shared.FieldError{Source: source, Detail: "field is required"})
+	return flatten(
+		validateIsValid("/type", lpa.Type),
+		required("/donor/firstNames", lpa.Donor.FirstNames),
+		required("/donor/lastName", lpa.Donor.LastName),
+		validateDate("/donor/dateOfBirth", lpa.Donor.DateOfBirth),
+		validateAddress("/donor/address", lpa.Donor.Address),
+		required("/certificateProvider/firstNames", lpa.CertificateProvider.FirstNames),
+		required("/certificateProvider/lastName", lpa.CertificateProvider.LastName),
+		validateAddress("/certificateProvider/address", lpa.CertificateProvider.Address),
+		validateIsValid("/certificateProvider/carryOutBy", lpa.CertificateProvider.CarryOutBy),
+		validateIf(lpa.CertificateProvider.CarryOutBy == shared.CarryOutByOnline,
+			required("/certificateProvider/email", lpa.CertificateProvider.Email)),
+		validateAttorneys("/attorneys", lpa.Attorneys),
+		validateIfElse(activeAttorneyCount == 0,
+			validateUnset("/howAttorneysMakeDecisions", lpa.HowAttorneysMakeDecisions),
+			validateIsValid("/howAttorneysMakeDecisions", lpa.HowAttorneysMakeDecisions)),
+		validateIf(lpa.HowAttorneysMakeDecisions == shared.HowMakeDecisionsJointlyForSomeSeverallyForOthers,
+			required("/howAttorneysMakeDecisionsDetails", lpa.HowAttorneysMakeDecisionsDetails)),
+		validateIf(lpa.HowAttorneysMakeDecisions == shared.HowMakeDecisionsJointlyAndSeverally,
+			validateIsValid("/howReplacementAttorneysStepIn", lpa.HowReplacementAttorneysStepIn)),
+		validateIf(lpa.HowReplacementAttorneysStepIn == shared.HowStepInAnotherWay,
+			required("/howReplacementAttorneysStepInDetails", lpa.HowReplacementAttorneysStepInDetails)),
+		validateIfElse(replacementAttorneyCount > 1 && (lpa.HowReplacementAttorneysStepIn == shared.HowStepInAllCanNoLongerAct || lpa.HowAttorneysMakeDecisions != shared.HowMakeDecisionsJointlyAndSeverally),
+			validateIsValid("/howReplacementAttorneysMakeDecisions", lpa.HowReplacementAttorneysMakeDecisions),
+			validateUnset("/howReplacementAttorneysMakeDecisions", lpa.HowReplacementAttorneysMakeDecisions)),
+		validateIf(lpa.HowReplacementAttorneysMakeDecisions == shared.HowMakeDecisionsJointlyForSomeSeverallyForOthers,
+			required("/howReplacementAttorneysMakeDecisionsDetails", lpa.HowReplacementAttorneysMakeDecisionsDetails)),
+		validateIf(lpa.Type == "hw",
+			validateIsValid("/lifeSustainingTreatmentOption", lpa.LifeSustainingTreatmentOption)),
+		validateIf(lpa.Type == "pfa",
+			validateIsValid("/whenTheLpaCanBeUsed", lpa.WhenTheLpaCanBeUsed)),
+		validateTime("/signedAt", lpa.SignedAt),
+	)
+}
+
+func countAttorneys(as []shared.Attorney) (actives, replacements int) {
+	for _, a := range as {
+		switch a.Status {
+		case shared.AttorneyStatusActive:
+			actives++
+		case shared.AttorneyStatusReplacement:
+			replacements++
 		}
 	}
+
+	return actives, replacements
+}
+
+func flatten(fieldErrors ...[]shared.FieldError) []shared.FieldError {
+	var errors []shared.FieldError
+
+	for _, e := range fieldErrors {
+		if e != nil {
+			errors = append(errors, e...)
+		}
+	}
+
+	return errors
+}
+
+func validateIf(ok bool, e []shared.FieldError) []shared.FieldError {
+	if ok {
+		return e
+	}
+
+	return nil
+}
+
+func validateIfElse(ok bool, eIf []shared.FieldError, eElse []shared.FieldError) []shared.FieldError {
+	if ok {
+		return eIf
+	}
+
+	return eElse
+}
+
+func required(source string, value string) []shared.FieldError {
+	if value == "" {
+		return []shared.FieldError{{Source: source, Detail: "field is required"}}
+	}
+
+	return nil
+}
+
+func validateDate(source string, date shared.Date) []shared.FieldError {
+	if date.IsMalformed {
+		return []shared.FieldError{{Source: source, Detail: "invalid format"}}
+	}
+
+	if date.IsZero() {
+		return []shared.FieldError{{Source: source, Detail: "field is required"}}
+	}
+
+	return nil
+}
+
+func validateTime(source string, t time.Time) []shared.FieldError {
+	if t.IsZero() {
+		return []shared.FieldError{{Source: source, Detail: "field is required"}}
+	}
+
+	return nil
+}
+
+func validateAddress(prefix string, address shared.Address) []shared.FieldError {
+	errors := flatten(
+		required(fmt.Sprintf("%s/line1", prefix), address.Line1),
+		required(fmt.Sprintf("%s/town", prefix), address.Town),
+		required(fmt.Sprintf("%s/country", prefix), address.Country),
+	)
 
 	if ok, _ := regexp.MatchString("^[A-Z]{2}$", address.Country); !ok {
 		errors = append(errors, shared.FieldError{Source: fmt.Sprintf("%s/country", prefix), Detail: "must be a valid ISO-3166-1 country code"})
@@ -27,64 +129,54 @@ func validateAddress(address shared.Address, prefix string, errors []shared.Fiel
 	return errors
 }
 
-func validateAttorney(attorney shared.Attorney, prefix string, errors []shared.FieldError) []shared.FieldError {
-	requiredFields := map[string]string{
-		fmt.Sprintf("%s/firstNames", prefix): attorney.FirstNames,
-		fmt.Sprintf("%s/surname", prefix):    attorney.Surname,
-		fmt.Sprintf("%s/status", prefix):     string(attorney.Status),
+type isValid interface {
+	~string
+	IsValid() bool
+}
+
+func validateIsValid[V isValid](source string, v V) []shared.FieldError {
+	if e := required(source, string(v)); e != nil {
+		return e
 	}
 
-	for source, value := range requiredFields {
-		if value == "" {
-			errors = append(errors, shared.FieldError{Source: source, Detail: "field is required"})
+	if !v.IsValid() {
+		return []shared.FieldError{{Source: source, Detail: "invalid value"}}
+	}
+
+	return nil
+}
+
+func validateUnset(source string, v interface{ Unset() bool }) []shared.FieldError {
+	if !v.Unset() {
+		return []shared.FieldError{{Source: source, Detail: "field must not be provided"}}
+	}
+
+	return nil
+}
+
+func validateAttorneys(prefix string, attorneys []shared.Attorney) []shared.FieldError {
+	var errors []shared.FieldError
+
+	if len(attorneys) == 0 {
+		return []shared.FieldError{{Source: prefix, Detail: "at least one attorney is required"}}
+	}
+
+	for i, attorney := range attorneys {
+		if e := validateAttorney(fmt.Sprintf("%s/%d", prefix, i), attorney); e != nil {
+			errors = append(errors, e...)
 		}
 	}
-
-	if attorney.DateOfBirth.IsMalformed {
-		errors = append(errors, shared.FieldError{Source: fmt.Sprintf("%s/dateOfBirth", prefix), Detail: "invalid format"})
-	} else if attorney.DateOfBirth.IsZero() {
-		errors = append(errors, shared.FieldError{Source: fmt.Sprintf("%s/dateOfBirth", prefix), Detail: "field is required"})
-	}
-
-	if !attorney.Status.IsValid() {
-		errors = append(errors, shared.FieldError{Source: fmt.Sprintf("%s/status", prefix), Detail: "invalid value"})
-	}
-
-	errors = validateAddress(attorney.Address, fmt.Sprintf("%s/address", prefix), errors)
 
 	return errors
 }
 
-func Validate(lpa shared.LpaInit) []shared.FieldError {
-	errors := []shared.FieldError{}
-
-	requiredFields := map[string]string{
-		"/donor/firstNames": lpa.Donor.FirstNames,
-		"/donor/surname":    lpa.Donor.Surname,
-	}
-
-	for source, value := range requiredFields {
-		if value == "" {
-			errors = append(errors, shared.FieldError{Source: source, Detail: "field is required"})
-		}
-	}
-
-	if lpa.Donor.DateOfBirth.IsMalformed {
-		errors = append(errors, shared.FieldError{Source: "/donor/dateOfBirth", Detail: "invalid format"})
-	} else if lpa.Donor.DateOfBirth.IsZero() {
-		errors = append(errors, shared.FieldError{Source: "/donor/dateOfBirth", Detail: "field is required"})
-	}
-
-	errors = validateAddress(lpa.Donor.Address, "/donor/address", errors)
-
-	if len(lpa.Attorneys) == 0 {
-		errors = append(errors, shared.FieldError{Source: "/attorneys", Detail: "at least one attorney is required"})
-	}
-
-	for index, attorney := range lpa.Attorneys {
-		prefix := fmt.Sprintf("/attorneys/%d", index)
-		errors = validateAttorney(attorney, prefix, errors)
-	}
-
-	return errors
+func validateAttorney(prefix string, attorney shared.Attorney) []shared.FieldError {
+	return flatten(
+		required(fmt.Sprintf("%s/firstNames", prefix), attorney.FirstNames),
+		required(fmt.Sprintf("%s/lastName", prefix), attorney.LastName),
+		required(fmt.Sprintf("%s/status", prefix), string(attorney.Status)),
+		validateDate(fmt.Sprintf("%s/dateOfBirth", prefix), attorney.DateOfBirth),
+		validateAddress(fmt.Sprintf("%s/address", prefix), attorney.Address),
+		validateIsValid(fmt.Sprintf("%s/status", prefix), attorney.Status),
+	)
 }
