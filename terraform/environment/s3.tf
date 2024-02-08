0176ee827512f0486aa3da4eec0d6d63a52d30dd
Andy Price <sixdaysandy@gmail.com>
2024-02-08 14:50:13 +0000

    VEGA-2289 - Add Cross Account S3 Backup #minor

--- terraform/environment/s3.tf
+++ terraform/environment/s3.tf
@@ -19,10 +19,10 @@ data "aws_iam_policy_document" "s3_replication_role_assume_role" {
 }
 
 module "s3_lpa_store_static_eu_west_1" {
-  source = "../modules/s3_multi_region_replica_bucket"
-  # accounts_allowed_to_read = [local.backup_account]
-  bucket_name   = "opg-lpa-store-static-${local.environment_name}-eu-west-1"
-  force_destroy = local.is_ephemeral
+  source                   = "../modules/s3_multi_region_replica_bucket"
+  accounts_allowed_to_read = [local.backup_account_id]
+  bucket_name              = "opg-lpa-store-static-${local.environment_name}-eu-west-1"
+  force_destroy            = local.is_ephemeral
   replication_configuration = concat(
     [{
       account_id  = data.aws_caller_identity.current.account_id,
@@ -41,10 +41,10 @@ module "s3_lpa_store_static_eu_west_1" {
 }
 
 module "s3_lpa_store_static_eu_west_2" {
-  source = "../modules/s3_multi_region_replica_bucket"
-  # accounts_allowed_to_read = [local.backup_account]
-  bucket_name   = "opg-lpa-store-static-${local.environment_name}-eu-west-2"
-  force_destroy = local.is_ephemeral
+  source                   = "../modules/s3_multi_region_replica_bucket"
+  accounts_allowed_to_read = [local.backup_account_id]
+  bucket_name              = "opg-lpa-store-static-${local.environment_name}-eu-west-2"
+  force_destroy            = local.is_ephemeral
   replication_configuration = concat(
     [{
       account_id  = data.aws_caller_identity.current.account_id,
@@ -62,27 +62,26 @@ module "s3_lpa_store_static_eu_west_2" {
   }
 }
 
-# module "s3_data_store_backup_account" {
-#   count                           = local.environment.features.cross_account_backup_enabled ? 1 : 0
-#   source                          = "../modules/s3_cross_account_backup"
-#   bucket_name                     = "${local.environment_name}.backup.sirius.opg.service.justice.gov.uk"
-#   environment_name                = local.environment_name
-#   force_destroy                   = local.is_ephemeral
-#   s3_access_logging_bucket_prefix = "s3-access-logs-opg-sirius-backup"
-#   s3_replication_role             = aws_iam_role.s3_replication_role
-#   providers = {
-#     aws.backup-account = aws.backup
-#     aws.source-account = aws
-#   }
-# }
+module "s3_data_store_backup_account" {
+  count                           = local.cross_account_backup_enabled ? 1 : 0
+  source                          = "../modules/s3_cross_account_backup"
+  bucket_name                     = "opg-lpa-store-static-${local.environment_name}-backup-eu-west-2"
+  environment_name                = local.environment_name
+  force_destroy                   = local.is_ephemeral
+  s3_access_logging_bucket_prefix = "s3-access-logs-opg-opg-backups-opg-backups"
+  s3_replication_role             = aws_iam_role.s3_replication_role
+  providers = {
+    aws.backup-account = aws.opg_backup
+    aws.source-account = aws.global
+  }
+}
 
 locals {
-  cross_account_backup_enabled    = false
-  cross_account_s3_replica_config = [] #local.cross_account_backup_enabled ? [
-  #   {
-  #     account_id  = "local.backup_account",
-  #     bucket      = module.s3_data_store_backup_account[0].bucket
-  #     kms_key_arn = module.s3_data_store_backup_account[0].kms_key.arn
-  #   }
-  # ] : []
+  cross_account_s3_replica_config = local.cross_account_backup_enabled ? [
+    {
+      account_id  = local.backup_account_id,
+      bucket      = module.s3_data_store_backup_account[0].bucket
+      kms_key_arn = module.s3_data_store_backup_account[0].kms_key.arn
+    }
+  ] : []
 }
