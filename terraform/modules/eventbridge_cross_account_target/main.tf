d897381e67c052efa13e7a7a7986c6fb34a3c716
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2024-02-13 10:42:01 +0000

    Add EventBridge infrastructure
    Includes:
    - Event bus in LPA Store account
    - Cross-account rule to send all locally-sourced events to the Sirius event bus
    - Dead letter-queue to catch events that couldn't be sent
    
    Fixes VEGA-2333 #minor

---
+++ terraform/modules/eventbridge_cross_account_target/main.tf
@@ -0,0 +1,48 @@
+resource "aws_cloudwatch_event_target" "cross_account_event_bus" {
+  target_id      = "cross-account-put-${var.name_suffix}"
+  event_bus_name = var.rule.event_bus_name
+  rule           = var.rule.name
+  arn            = var.target_event_bus_arn
+  role_arn       = aws_iam_role.cross_account_put.arn
+
+  dead_letter_config {
+    arn = var.dead_letter_queue_arn
+  }
+}
+
+resource "aws_iam_role" "cross_account_put" {
+  name               = "cross-account-put-${var.name_suffix}"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy" "cross_account_put" {
+  name   = "cross-account-put-${var.name_suffix}"
+  policy = data.aws_iam_policy_document.cross_account_put_access.json
+  role   = aws_iam_role.cross_account_put.id
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "cross_account_put_access" {
+  statement {
+    sid    = "CrossAccountPutAccess"
+    effect = "Allow"
+    actions = [
+      "events:PutEvents",
+    ]
+    resources = [
+      var.target_event_bus_arn
+    ]
+  }
+}
