a35ef98799f9f5115a20c7d37ee2bc7867965dd8
Greg Tyler <gregtyler@users.noreply.github.com>
2024-02-13 11:22:43 +0000

    Move DynamoDB into localstack (#106)
    No point having extra containers, localstack is familiar and natively supports initialisation and healthchecks.
    
    For VEGA-2333 #patch

--- localstack/wait/healthcheck.sh
+++ localstack/wait/healthcheck.sh
@@ -4,3 +4,8 @@
 buckets=$(awslocal s3 ls)
 
 echo $buckets | grep "opg-lpa-store-static-eu-west-1" || exit 1
+
+# DynamoDB
+tables=$(awslocal dynamodb list-tables)
+echo $tables | grep '"deeds"' || exit 1
+echo $tables | grep '"changes"' || exit 1
