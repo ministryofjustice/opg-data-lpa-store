b79be753ca8190f93d6ec1bf8f1d9e93a95c0e04
Mabel Li <mabel.li@digital.justice.gov.uk>
2024-02-19 21:00:42 +0000

    Refactor client unit tests

--- internal/event/client_test.go
+++ internal/event/client_test.go
@@ -10,14 +10,14 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/eventbridge"
 	"github.com/aws/aws-sdk-go-v2/service/eventbridge/types"
 	"github.com/stretchr/testify/assert"
-	mock "github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/mock"
 )
 
-type mockEventBridgeClient struct {
+type mockEventbridgeClient struct {
 	mock.Mock
 }
 
-func (_m *mockEventBridgeClient) PutEvents(ctx context.Context, params *eventbridge.PutEventsInput, optFns ...func(*eventbridge.Options)) (*eventbridge.PutEventsOutput, error) {
+func (_m *mockEventbridgeClient) PutEvents(ctx context.Context, params *eventbridge.PutEventsInput, optFns ...func(*eventbridge.Options)) (*eventbridge.PutEventsOutput, error) {
 	var r0 *eventbridge.PutEventsOutput
 	var r1 error = errors.New("err")
 
@@ -28,34 +28,25 @@ func TestClientSendEvent(t *testing.T) {
 	ctx := context.Background()
 	expectedError := errors.New("err")
 
-	testcases := map[string]func() (func(*Client) error, any) {
-		"lpa-updated": func() (func(*Client) error, any) {
-			event := LpaUpdated{ uid: "M-1234-1234-1234", changeType: "CREATED" }
+	event := LpaUpdated{ Uid: "M-1234-1234-1234", ChangeType: "CREATED" }
 
-			return func(client *Client) error { return client.SendLpaUpdated(ctx, event) }, event
-		},
-	}
+	fn, event := func(client *Client) error { return client.SendLpaUpdated(ctx, event) }, event
 
-	for eventName, setup := range testcases {
-		t.Run(eventName, func(t *testing.T) {
-			fn, event := setup()
-			data, _ := json.Marshal(event)
+	data, _ := json.Marshal(event)
 
-			mockClient := &mockEventBridgeClient{}
-			mockClient.On("PutEvents", mock.Anything, &eventbridge.PutEventsInput{
-					Entries: []types.PutEventsRequestEntry{{
-						EventBusName: aws.String("my-bus"),
-						Source:       aws.String("opg.poas.lpastore"),
-						DetailType:   aws.String(eventName),
-						Detail:       aws.String(string(data)),
-					}},
-				}).
-				Return(nil, expectedError)
+	mockClient := &mockEventbridgeClient{}
+	mockClient.On("PutEvents", mock.Anything, &eventbridge.PutEventsInput{
+			Entries: []types.PutEventsRequestEntry{{
+				EventBusName: aws.String("my-bus"),
+				Source:       aws.String("opg.poas.lpastore"),
+				DetailType:   aws.String("lpa-updated"),
+				Detail:       aws.String(string(data)),
+			}},
+		}).
+		Return(nil, expectedError)
 
-			svc := &Client{svc: mockClient, eventBusName: "my-bus"}
-			err := fn(svc)
+	svc := &Client{svc: mockClient, eventBusName: "my-bus"}
+	err := fn(svc)
 
-			assert.Equal(t, expectedError, err)
-		})
-	}
+	assert.Equal(t, expectedError, err)
 }
