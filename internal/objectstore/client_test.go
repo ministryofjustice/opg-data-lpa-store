49b4145845f0deb57c9b5c7817e1560d7a7191d4
Elliot Smith <elliot.smith@justice.digital.gov.uk>
2024-03-01 15:57:36 +0000

    Fix broken tests

--- internal/objectstore/client_test.go
+++ internal/objectstore/client_test.go
@@ -18,36 +18,16 @@ func (m *mockAwsClient) PutObject(ctx context.Context, input *s3.PutObjectInput,
 	return args.Get(0).(*s3.PutObjectOutput), args.Error(1)
 }
 
-func (m *mockAwsClient) GetObject(ctx context.Context, input *s3.GetObjectInput, opts ...func(*s3.Options)) (*s3.GetObjectOutput, error) {
-	args := m.Called(ctx, input)
-	return args.Get(0).(*s3.GetObjectOutput), args.Error(1)
-}
-
 func TestPut(t *testing.T) {
 	client := mockAwsClient{}
-	client.On("PutObject", mock.Anything, mock.Anything).Return(&s3.PutObjectOutput{}, nil)
+	client.On("PutObject", mock.Anything, mock.Anything, mock.Anything).Return(&s3.PutObjectOutput{}, nil)
 
 	c := S3Client{
 		bucketName: "bucket1",
 		awsClient:  &client,
 	}
 
-	_, err := c.Put("anobject", struct{ ID int }{ID: 1})
-
-	assert.Equal(t, nil, err)
-	client.AssertExpectations(t)
-}
-
-func TestGet(t *testing.T) {
-	client := mockAwsClient{}
-	client.On("GetObject", mock.Anything, mock.Anything).Return(&s3.GetObjectOutput{}, nil)
-
-	c := S3Client{
-		bucketName: "bucket1",
-		awsClient:  &client,
-	}
-
-	_, err := c.Get("anotherobject")
+	err := c.Put(context.Background(), "anobject", struct{ ID int }{ID: 1})
 
 	assert.Equal(t, nil, err)
 	client.AssertExpectations(t)
