f9dc7fdb924f92d3a84df00cfbdba1d3dba9aac3
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2024-03-22 18:27:58 +0000

    Add ECS Terraform
    #minor

---
+++ terraform/modules/fixtures_service/security_group.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "ecs" {
+  name   = "fixtures-${var.environment_name}"
+  vpc_id = var.vpc_id
+
+  provider = aws.region
+}
+
+resource "aws_security_group_rule" "ecs_to_vpc_endpoint" {
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 443
+  to_port                  = 443
+  security_group_id        = aws_security_group.ecs.id
+  source_security_group_id = data.aws_security_group.vpc_endpoints_application.id
+
+  provider = aws.region
+}
+
+data "aws_security_group" "vpc_endpoints_application" {
+  vpc_id = var.vpc_id
+  name   = "vpc-endpoint-access-application-subnets-${data.aws_region.current.name}"
+
+  provider = aws.region
+}
+
+# Required to pull image from ECR
+resource "aws_security_group_rule" "ecs_to_global" {
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  security_group_id = aws_security_group.ecs.id
+  cidr_blocks       = ["0.0.0.0/0"]
+
+  provider = aws.region
+}
