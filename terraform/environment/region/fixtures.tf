9005a8897842e3e6db7f2deb2783f874e849a7cf
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2024-03-22 17:58:32 +0000

    Add ECS Terraform
    #minor

---
+++ terraform/environment/region/fixtures.tf
@@ -0,0 +1,23 @@
+module "fixtures" {
+  count  = var.has_fixtures ? 1 : 0
+  source = "../../modules/fixtures_service"
+
+  cloudwatch_kms_key_id = aws_kms_key.cloudwatch.arn
+  ecr_image_uri         = "${data.aws_ecr_repository.fixtures[0].repository_url}:${var.app_version}"
+  environment_name      = var.environment_name
+  service_url           = local.domain_name
+  subnet_ids            = data.aws_subnets.application.ids
+  vpc_id                = data.aws_vpc.main.id
+
+  providers = {
+    aws.global     = aws.global
+    aws.management = aws.management
+    aws.region     = aws.region
+  }
+}
+
+data "aws_ecr_repository" "fixtures" {
+  count    = var.has_fixtures ? 1 : 0
+  name     = "lpa-store/fixtures"
+  provider = aws.management
+}
