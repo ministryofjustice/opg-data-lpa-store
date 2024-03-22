45c057e9d5eacb7c7bd3316689e4257397591b3b
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2024-03-22 12:55:45 +0000

    Create VPC endpoints + security group
    So that Lambda and ECS inside the VPC can connect to AWS services
    
    For CTC-140 #minor

--- terraform/account/region/vpc.tf
+++ terraform/account/region/vpc.tf
@@ -9,3 +9,42 @@ module "vpc" {
     aws = aws.region
   }
 }
+
+resource "aws_security_group" "vpc_endpoints_application" {
+  name   = "vpc-endpoint-access-application-subnets-${data.aws_region.current.name}"
+  vpc_id = module.vpc.vpc.id
+}
+
+resource "aws_security_group_rule" "vpc_endpoints_application_subnet_ingress" {
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.vpc_endpoints_application.id
+  type              = "ingress"
+  cidr_blocks       = module.vpc.application_subnets[*].cidr_block
+  description       = "Allow Services in Private Subnets of ${data.aws_region.current.name} to connect to VPC Interface Endpoints"
+}
+
+locals {
+  interface_endpoint = toset([
+    "dynamodb",
+    "ecr.api",
+    "ecr.dkr",
+    "execute-api",
+    "logs",
+    "s3",
+    "secretsmanager",
+    "xray",
+  ])
+}
+
+resource "aws_vpc_endpoint" "application" {
+  for_each = local.interface_endpoint
+
+  vpc_id              = module.vpc.vpc.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.${each.value}"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  security_group_ids  = aws_security_group.vpc_endpoints_application[*].id
+  subnet_ids          = module.vpc.application_subnets[*].id
+}
