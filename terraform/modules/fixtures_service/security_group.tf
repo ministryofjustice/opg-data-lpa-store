2202729edebb1c3ae65b2d129f8babfc8ae239ea
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2024-03-25 14:13:31 +0000

    Add load balancer to ECS service configuration
    And configure security groups properly

--- terraform/modules/fixtures_service/security_group.tf
+++ terraform/modules/fixtures_service/security_group.tf
@@ -5,6 +5,16 @@ resource "aws_security_group" "ecs" {
   provider = aws.region
 }
 
+resource "aws_security_group_rule" "alb_ingress" {
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 80
+  to_port                  = 80
+  source_security_group_id = aws_security_group.loadbalancer_gov_wifi.id
+  security_group_id        = aws_security_group.ecs.id
+  description              = "Inbound from the ALB"
+}
+
 resource "aws_security_group_rule" "ecs_to_vpc_endpoint" {
   type                     = "egress"
   protocol                 = "tcp"
