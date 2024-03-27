a9234ac452b5e873b3dbb0120f38a51ad781d515
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2024-03-27 11:01:59 +0000

    Use Secrets Manager for JWT secret
    Rather than passing it in as an easily-leakable environment variable, use Secrets Manager to store the secret used for JWT key signing
    
    For VEGA-2424 #minor

--- terraform/modules/lambda/iam.tf
+++ terraform/modules/lambda/iam.tf
@@ -54,6 +54,19 @@ data "aws_iam_policy_document" "lambda" {
       "events:PutEvents"
     ]
   }
+
+  statement {
+    sid       = "allowReadJwtSecret"
+    effect    = "Allow"
+    resources = [aws_secretsmanager_secret.jwt_secret_key.arn]
+    actions = [
+      "secretsmanager:GetSecretValue"
+    ]
+  }
+}
+
+data "aws_secretsmanager_secret" "jwt_secret_key" {
+  name = "${var.account_name}/jwt-key"
 }
 
 resource "aws_lambda_permission" "allow_lambda_execution_operator" {
