29fec50480e200a98cb6ea34a72685810d0688cf
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2024-04-10 10:55:45 +0100

    Add visual editor to fixtures
    Use the JSON schema to generate a bunch of nested fields so that users don't need to understand JSON in order to create fixture data in the store.
    
    For now I've hard-coded the current JSON schema for ease, but I'm hoping to work on formalising all the schemas properly soon.
    
    Fixes CTC-145 #minor

--- fixtures/static/js/main.mjs
+++ fixtures/static/js/main.mjs
@@ -1,6 +1,8 @@
+import { JsonSchemaEditor } from "./json-schema-editor.mjs";
 import { UidGenerator } from "./uid-generator.mjs";
 
 const initiators = {
+  "json-schema-editor": JsonSchemaEditor,
   "uid-generator": UidGenerator,
 };
 
@@ -8,6 +10,6 @@ export function initAll() {
   Object.entries(initiators).forEach(([name, Component]) => {
     const $elements = document.querySelectorAll(`[data-module="${name}"]`);
 
-    $elements.forEach(($element) => new UidGenerator($element));
+    $elements.forEach(($element) => new Component($element));
   });
 }
