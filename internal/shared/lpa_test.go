4dcdf8616a02f1df241730a1e18efa5933f57916
Mabel Li <mabel.li@digital.justice.gov.uk>
2024-04-29 16:48:56 +0100

    Remove more tests that assert town as required

--- internal/shared/lpa_test.go
+++ internal/shared/lpa_test.go
@@ -10,9 +10,9 @@ import (
 func TestLpaInitMarshalJSON(t *testing.T) {
 	expected := `{
 "lpaType":"",
-"donor":{"uid":"","firstNames":"","lastName":"","address":{"line1":"","town":"","country":""},"dateOfBirth":"","email":"","contactLanguagePreference":""},
+"donor":{"uid":"","firstNames":"","lastName":"","address":{"line1":"","country":""},"dateOfBirth":"","email":"","contactLanguagePreference":""},
 "attorneys":null,
-"certificateProvider":{"uid":"","firstNames":"","lastName":"","address":{"line1":"","town":"","country":""},"email":"","phone":"","channel":""},
+"certificateProvider":{"uid":"","firstNames":"","lastName":"","address":{"line1":"","country":""},"email":"","phone":"","channel":""},
 "signedAt":"0001-01-01T00:00:00Z"
 }`
 
