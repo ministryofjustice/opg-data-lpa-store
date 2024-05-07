fd3652461974b09f73b4162f77d124ec5071ae7e
Alex Saunders <acsauk@gmail.com>
2024-05-07 16:49:33 +0100

    support CP opt out

---
+++ lambda/update/certificate_provider_opt_out.go
@@ -0,0 +1,28 @@
+package main
+
+import "github.com/ministryofjustice/opg-data-lpa-store/internal/shared"
+
+type CertificateProviderOptOut struct{}
+
+func (c CertificateProviderOptOut) Apply(lpa *shared.Lpa) []shared.FieldError {
+	emptyCertificateProvider := shared.CertificateProvider{}
+	if lpa.CertificateProvider == emptyCertificateProvider {
+		return []shared.FieldError{{Source: "/type", Detail: "certificate provider not present on LPA"}}
+	}
+
+	if lpa.CertificateProvider.SignedAt != nil && !lpa.CertificateProvider.SignedAt.IsZero() {
+		return []shared.FieldError{{Source: "/type", Detail: "certificate provider cannot opt out after providing certificate"}}
+	}
+
+	lpa.CertificateProvider = shared.CertificateProvider{}
+
+	return nil
+}
+
+func validateCertificateProviderOptOut(changes []shared.Change) (CertificateProviderOptOut, []shared.FieldError) {
+	if len(changes) > 0 {
+		return CertificateProviderOptOut{}, []shared.FieldError{{Source: "/changes", Detail: "expected empty"}}
+	}
+
+	return CertificateProviderOptOut{}, nil
+}
---
+++ lambda/update/certificate_provider_opt_out_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/ministryofjustice/opg-data-lpa-store/internal/shared"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCertificateProviderOptOutApply(t *testing.T) {
+	lpa := &shared.Lpa{LpaInit: shared.LpaInit{CertificateProvider: shared.CertificateProvider{Email: "a@example"}}}
+	c := CertificateProviderOptOut{}
+
+	errors := c.Apply(lpa)
+	assert.Empty(t, errors)
+	assert.Equal(t, shared.CertificateProvider{}, lpa.CertificateProvider)
+}
+
+func TestCertificateProviderOptOutApplyWhenNoCertificateProvider(t *testing.T) {
+	lpa := &shared.Lpa{LpaInit: shared.LpaInit{
+		CertificateProvider: shared.CertificateProvider{}},
+	}
+
+	errors := CertificateProviderOptOut{}.Apply(lpa)
+	assert.Equal(t, errors, []shared.FieldError{{Source: "/type", Detail: "certificate provider not present on LPA"}})
+}
+
+func TestCertificateProviderOptOutApplyWhenCertificateProvided(t *testing.T) {
+	now := time.Now()
+	certificateProvider := shared.CertificateProvider{Email: "a@example", SignedAt: &now}
+	lpa := &shared.Lpa{LpaInit: shared.LpaInit{
+		CertificateProvider: certificateProvider},
+	}
+
+	errors := CertificateProviderOptOut{}.Apply(lpa)
+	assert.Equal(t, errors, []shared.FieldError{{Source: "/type", Detail: "certificate provider cannot opt out after providing certificate"}})
+	assert.Equal(t, certificateProvider, lpa.CertificateProvider)
+}
