8a483d66e338213a0147819f2164f502b826bcfd
Alex Saunders <acsauk@gmail.com>
2024-05-09 10:26:23 +0100

    MLPAB-1269: Support CERTIFICATE_PROVIDER_OPT_OUT update (#195)

--- lambda/update/validate.go
+++ lambda/update/validate.go
@@ -10,16 +10,18 @@ type Applyable interface {
 
 func validateUpdate(update shared.Update, lpa *shared.Lpa) (Applyable, []shared.FieldError) {
 	switch update.Type {
-	case "CERTIFICATE_PROVIDER_SIGN":
-		return validateCertificateProviderSign(update.Changes, lpa)
 	case "ATTORNEY_SIGN":
 		return validateAttorneySign(update.Changes, lpa)
-	case "TRUST_CORPORATION_SIGN":
-		return validateTrustCorporationSign(update.Changes, lpa)
+	case "CERTIFICATE_PROVIDER_OPT_OUT":
+		return validateCertificateProviderOptOut(update.Changes)
+	case "CERTIFICATE_PROVIDER_SIGN":
+		return validateCertificateProviderSign(update.Changes, lpa)
 	case "PERFECT":
 		return validatePerfect(update.Changes)
 	case "REGISTER":
 		return validateRegister(update.Changes)
+	case "TRUST_CORPORATION_SIGN":
+		return validateTrustCorporationSign(update.Changes, lpa)
 	default:
 		return nil, []shared.FieldError{{Source: "/type", Detail: "invalid value"}}
 	}
