0b9615d3e9cc6a16278ef176eec50f4d3222a86e
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-06-26 12:18:37 +0100

    MLPAB-2189 - Create a replicated secret in management account (#223)

---
+++ terraform/account/data_sources.tf
@@ -0,0 +1,11 @@
+data "aws_default_tags" "default" {
+  provider = aws.eu_west_1
+}
+
+data "aws_region" "eu_west_1" {
+  provider = aws.eu_west_1
+}
+
+data "aws_region" "eu_west_2" {
+  provider = aws.eu_west_2
+}
