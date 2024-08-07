cad487a61bd9555c3c41451ccc934ddde07b98d6
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-08-07 19:50:08 +0100

    use same name for secret and kms alias

--- terraform/account/kms_key_jwt_secret.tf
+++ terraform/account/kms_key_jwt_secret.tf
@@ -1,7 +1,7 @@
 module "jwt_kms" {
   source                  = "../modules/kms_key"
   encrypted_resource      = "jwt key secret"
-  kms_key_alias_name      = "${data.aws_default_tags.default.tags.application}-jwt-key-secret-encryption"
+  kms_key_alias_name      = "${data.aws_default_tags.default.tags.application}/${data.aws_default_tags.default.tags.account}/jwt-key"
   enable_key_rotation     = true
   enable_multi_region     = true
   deletion_window_in_days = 10
