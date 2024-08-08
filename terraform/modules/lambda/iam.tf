adc93b276240ffe0d01838c62097b37aadca7036
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-08-08 14:00:23 +0100

    allow mlpab ap and lambda to fetch jwt

--- terraform/modules/lambda/iam.tf
+++ terraform/modules/lambda/iam.tf
@@ -1,7 +1,12 @@
 resource "aws_iam_role" "lambda" {
   name = "lambda-${var.lambda_name}-${var.environment_name}-${data.aws_region.current.name}"
+  # this path will be used to grant permission to the lambda to access the KMS key
   # path               = "/lpa-store-lambda/"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 data "aws_iam_policy_document" "lambda_assume" {
