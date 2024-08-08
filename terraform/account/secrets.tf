aa19c96ef65f696880da61dfe51372e478044754
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-08-08 16:34:38 +0100

    MLPAB-2237 - remove policy and encryption (#240)
    * remove policy and encryption
    
    * remove cross account access from kms key

--- terraform/account/secrets.tf
+++ terraform/account/secrets.tf
@@ -1,17 +1,18 @@
 resource "aws_secretsmanager_secret" "jwt_key" {
   name        = "${data.aws_default_tags.default.tags.application}/${data.aws_default_tags.default.tags.account}/jwt-key"
   description = "JWT key for ${data.aws_default_tags.default.tags.application} in ${data.aws_default_tags.default.tags.account}, for use with Make and Register, and Use a LPA"
-  policy      = data.aws_iam_policy_document.jwt_key_cross_account_access.json
-  kms_key_id  = module.jwt_kms.eu_west_1_target_key_id
+  # policy      = data.aws_iam_policy_document.jwt_key_cross_account_access.json
+  # kms_key_id  = module.jwt_kms.eu_west_1_target_key_id
   replica {
-    region     = data.aws_region.eu_west_2.name
-    kms_key_id = module.jwt_kms.eu_west_2_target_key_id
+    region = data.aws_region.eu_west_2.name
+    # kms_key_id = module.jwt_kms.eu_west_2_target_key_id
   }
   provider = aws.management_eu_west_1
 }
 
 data "aws_iam_policy_document" "jwt_key_cross_account_access" {
   statement {
+    effect = "Allow"
     actions = [
       "secretsmanager:GetSecretValue",
     ]
@@ -20,7 +21,7 @@ data "aws_iam_policy_document" "jwt_key_cross_account_access" {
     ]
     principals {
       type        = "AWS"
-      identifiers = local.account.jwt_key_cross_account_access_roles
+      identifiers = tolist(local.account.jwt_key_cross_account_access_roles)
     }
   }
 }
