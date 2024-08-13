f7160112a26112b6808e4f241d01d0e5141e1341
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-08-13 11:49:14 +0100

    use new secret in fixtures

---
+++ terraform/modules/fixtures_service/data_sources.tf
@@ -0,0 +1,9 @@
+data "aws_secretsmanager_secret" "jwt_secret_key" {
+  name     = "${data.aws_default_tags.default.tags.application}/${data.aws_default_tags.default.tags.account}/jwt-key"
+  provider = aws.management
+}
+
+data "aws_kms_alias" "jwt_key" {
+  name     = "alias/${data.aws_default_tags.default.tags.application}/${data.aws_default_tags.default.tags.account}/jwt-key"
+  provider = aws.management
+}
