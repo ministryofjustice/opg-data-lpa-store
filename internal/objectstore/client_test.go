a27c222944031808b61ba199b9dcdd8ecfb93f7b
Joshua Hawxwell <m@hawx.me>
2024-08-23 10:05:24 +0100

    Use mockery for internal/objectstore testing

--- internal/objectstore/client_test.go
+++ internal/objectstore/client_test.go
@@ -14,98 +14,88 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-type mockAwsClient struct {
-	mock.Mock
-}
+var (
+	ctx           = context.WithValue(context.Background(), "for", "testing")
+	expectedError = errors.New("expected")
+	bucketName    = "a-bucket"
+	objectKey     = "an-object-key"
+)
 
-func (m *mockAwsClient) PutObject(ctx context.Context, input *s3.PutObjectInput, opts ...func(*s3.Options)) (*s3.PutObjectOutput, error) {
-	args := m.Called(ctx, input)
-	return args.Get(0).(*s3.PutObjectOutput), args.Error(1)
-}
+func TestS3ClientPut(t *testing.T) {
+	awsS3Client := newMockAwsS3Client(t)
+	awsS3Client.EXPECT().
+		PutObject(ctx, &s3.PutObjectInput{
+			Bucket:               aws.String(bucketName),
+			Key:                  aws.String(objectKey),
+			Body:                 bytes.NewReader([]byte(`{"ID":1}`)),
+			ServerSideEncryption: types.ServerSideEncryptionAwsKms,
+		}).
+		Return(nil, expectedError)
 
-func TestPut(t *testing.T) {
-	client := mockAwsClient{}
-	client.On("PutObject", mock.Anything, mock.Anything, mock.Anything).Return(&s3.PutObjectOutput{}, nil)
-
-	c := S3Client{
-		bucketName: "bucket1",
-		awsClient:  &client,
+	client := &S3Client{
+		bucketName: bucketName,
+		awsClient:  awsS3Client,
 	}
 
-	err := c.Put(context.Background(), "anobject", struct{ ID int }{ID: 1})
-
-	assert.Equal(t, nil, err)
-	client.AssertExpectations(t)
+	err := client.Put(ctx, objectKey, struct{ ID int }{ID: 1})
+	assert.Equal(t, expectedError, err)
 }
 
-func TestUploadFile(t *testing.T) {
+func TestS3ClientUploadFile(t *testing.T) {
 	upload := shared.FileUpload{
 		Filename: "myfile.txt",
 		Data:     "Q29udGVudHMgb2YgbXkgZmlsZQ==",
 	}
 
-	client := mockAwsClient{}
-	client.On("PutObject", mock.Anything, &s3.PutObjectInput{
-		Bucket:               aws.String("bucket1"),
-		Key:                  aws.String("dir/myfile.txt"),
-		Body:                 bytes.NewReader([]byte("Contents of my file")),
-		ServerSideEncryption: types.ServerSideEncryptionAwsKms,
-	}).Return(&s3.PutObjectOutput{}, nil)
+	awsS3Client := newMockAwsS3Client(t)
+	awsS3Client.EXPECT().
+		PutObject(ctx, &s3.PutObjectInput{
+			Bucket:               aws.String(bucketName),
+			Key:                  aws.String("dir/myfile.txt"),
+			Body:                 bytes.NewReader([]byte("Contents of my file")),
+			ServerSideEncryption: types.ServerSideEncryptionAwsKms,
+		}).
+		Return(&s3.PutObjectOutput{}, nil)
 
-	c := S3Client{
-		bucketName: "bucket1",
-		awsClient:  &client,
+	client := &S3Client{
+		bucketName: bucketName,
+		awsClient:  awsS3Client,
 	}
 
-	file, err := c.UploadFile(context.Background(), upload, "dir/myfile.txt")
-
+	file, err := client.UploadFile(ctx, upload, "dir/myfile.txt")
 	assert.Nil(t, err)
 	assert.Equal(t, "dir/myfile.txt", file.Path)
 	assert.Equal(t, "7ac4f2b48096ac5f4600a0775563d0f2b3369a3ea00d1fa813f45c18620dba28", file.Hash)
-	client.AssertExpectations(t)
 }
 
-func TestUploadFileDecodingError(t *testing.T) {
+func TestS3ClientUploadFileWhenDecodingError(t *testing.T) {
 	upload := shared.FileUpload{
 		Filename: "myfile.txt",
 		Data:     "This isn't base 64",
 	}
 
-	c := S3Client{
-		bucketName: "bucket1",
-		awsClient:  &mockAwsClient{},
-	}
+	client := &S3Client{}
 
-	file, err := c.UploadFile(context.Background(), upload, "dir/myfile.txt")
-
-	assert.Equal(t, file, shared.File{})
-	assert.Contains(t, err.Error(), "illegal base64 data")
+	_, err := client.UploadFile(ctx, upload, "dir/myfile.txt")
+	assert.ErrorContains(t, err, "illegal base64 data")
 }
 
-func TestUploadFileS3Error(t *testing.T) {
+func TestS3ClientUploadFileWhenS3Error(t *testing.T) {
 	upload := shared.FileUpload{
 		Filename: "myfile.txt",
 		Data:     "Q29udGVudHMgb2YgbXkgZmlsZQ==",
 	}
 
-	expectedErr := errors.New("could not save object")
+	awsS3Client := newMockAwsS3Client(t)
+	awsS3Client.EXPECT().
+		PutObject(mock.Anything, mock.Anything).
+		Return(nil, expectedError)
 
-	client := mockAwsClient{}
-	client.On("PutObject", mock.Anything, &s3.PutObjectInput{
-		Bucket:               aws.String("bucket1"),
-		Key:                  aws.String("dir/myfile.txt"),
-		Body:                 bytes.NewReader([]byte("Contents of my file")),
-		ServerSideEncryption: types.ServerSideEncryptionAwsKms,
-	}).Return(&s3.PutObjectOutput{}, expectedErr)
-
-	c := S3Client{
+	client := &S3Client{
 		bucketName: "bucket1",
-		awsClient:  &client,
+		awsClient:  awsS3Client,
 	}
 
-	file, err := c.UploadFile(context.Background(), upload, "dir/myfile.txt")
-
-	assert.Equal(t, file, shared.File{})
-	assert.Equal(t, expectedErr, err)
-	client.AssertExpectations(t)
+	_, err := client.UploadFile(ctx, upload, "dir/myfile.txt")
+	assert.Equal(t, expectedError, err)
 }
