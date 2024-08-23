17cd6e9835041787c5025335b545e3750ab8070f
Joshua Hawxwell <m@hawx.me>
2024-08-23 14:03:29 +0100

    Add tests for the easy lambdas

---
+++ lambda/get/main_test.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log/slog"
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/ministryofjustice/opg-data-lpa-store/internal/shared"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	ctx           = context.WithValue(context.Background(), "for", "testing")
+	expectedError = errors.New("err")
+)
+
+func TestLambdaHandleEvent(t *testing.T) {
+	req := events.APIGatewayProxyRequest{
+		PathParameters: map[string]string{"uid": "my-uid"},
+	}
+
+	lpa := shared.Lpa{Uid: "my-uid"}
+	body, _ := json.Marshal(lpa)
+
+	verifier := newMockVerifier(t)
+	verifier.EXPECT().
+		VerifyHeader(req).
+		Return(nil, nil)
+
+	logger := newMockLogger(t)
+	logger.EXPECT().
+		Debug("Successfully parsed JWT from event header")
+
+	store := newMockStore(t)
+	store.EXPECT().
+		Get(ctx, "my-uid").
+		Return(lpa, nil)
+
+	lambda := &Lambda{
+		verifier: verifier,
+		logger:   logger,
+		store:    store,
+	}
+
+	resp, err := lambda.HandleEvent(ctx, req)
+	assert.Nil(t, err)
+	assert.Equal(t, events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(body),
+	}, resp)
+}
+
+func TestLambdaHandleEventWhenUnauthorised(t *testing.T) {
+	req := events.APIGatewayProxyRequest{
+		PathParameters: map[string]string{"uid": "my-uid"},
+	}
+
+	verifier := newMockVerifier(t)
+	verifier.EXPECT().
+		VerifyHeader(req).
+		Return(nil, expectedError)
+
+	logger := newMockLogger(t)
+	logger.EXPECT().
+		Info("Unable to verify JWT from header")
+
+	lambda := &Lambda{
+		verifier: verifier,
+		logger:   logger,
+	}
+
+	resp, err := lambda.HandleEvent(ctx, req)
+	assert.Nil(t, err)
+	assert.Equal(t, events.APIGatewayProxyResponse{
+		StatusCode: 401,
+		Body:       `{"code":"UNAUTHORISED","detail":"Invalid JWT"}`,
+	}, resp)
+}
+
+func TestLambdaHandleEventWhenNotFound(t *testing.T) {
+	req := events.APIGatewayProxyRequest{
+		PathParameters: map[string]string{"uid": "my-uid"},
+	}
+
+	verifier := newMockVerifier(t)
+	verifier.EXPECT().
+		VerifyHeader(req).
+		Return(nil, nil)
+
+	logger := newMockLogger(t)
+	logger.EXPECT().
+		Debug("Successfully parsed JWT from event header")
+	logger.EXPECT().
+		Debug("Uid not found")
+
+	store := newMockStore(t)
+	store.EXPECT().
+		Get(ctx, "my-uid").
+		Return(shared.Lpa{}, nil)
+
+	lambda := &Lambda{
+		verifier: verifier,
+		logger:   logger,
+		store:    store,
+	}
+
+	resp, err := lambda.HandleEvent(ctx, req)
+	assert.Nil(t, err)
+	assert.Equal(t, events.APIGatewayProxyResponse{
+		StatusCode: 404,
+		Body:       `{"code":"NOT_FOUND","detail":"Record not found"}`,
+	}, resp)
+}
+
+func TestLambdaHandleEventWhenStoreErrors(t *testing.T) {
+	req := events.APIGatewayProxyRequest{
+		PathParameters: map[string]string{"uid": "my-uid"},
+	}
+
+	verifier := newMockVerifier(t)
+	verifier.EXPECT().
+		VerifyHeader(req).
+		Return(nil, nil)
+
+	logger := newMockLogger(t)
+	logger.EXPECT().
+		Debug("Successfully parsed JWT from event header")
+	logger.EXPECT().
+		Error("error fetching LPA", slog.Any("err", expectedError))
+
+	store := newMockStore(t)
+	store.EXPECT().
+		Get(ctx, "my-uid").
+		Return(shared.Lpa{}, expectedError)
+
+	lambda := &Lambda{
+		verifier: verifier,
+		logger:   logger,
+		store:    store,
+	}
+
+	resp, err := lambda.HandleEvent(ctx, req)
+	assert.Nil(t, err)
+	assert.Equal(t, events.APIGatewayProxyResponse{
+		StatusCode: 500,
+		Body:       `{"code":"INTERNAL_SERVER_ERROR","detail":"Internal server error"}`,
+	}, resp)
+}
