04b758a6d71917e553a2b0c2ac1c749dcee21e91
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2024-10-15 14:40:26 +0100

    Run fixtures on non-privileged port
    But it will still be providing 443 at the load balancer.
    
    #patch

--- terraform/modules/fixtures_service/load_balancer.tf
+++ terraform/modules/fixtures_service/load_balancer.tf
@@ -57,7 +57,7 @@ resource "aws_lb_listener_rule" "fixtures" {
 
 resource "aws_lb_target_group" "fixtures" {
   name                 = "fixtures-${var.environment_name}-http"
-  port                 = 80
+  port                 = var.container_port
   protocol             = "HTTP"
   target_type          = "ip"
   vpc_id               = var.vpc_id
