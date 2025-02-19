71f5e397a2f43df83fc4ba081d20d055eb1d60c9
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-02-19 10:27:14 +0000

    upgrade lock

--- terraform/environment/versions.tf
+++ terraform/environment/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.8.0"
+      version = "~> 5.87.0"
     }
   }
-  required_version = "= 1.10.5"
+  required_version = "1.10.5"
 }
