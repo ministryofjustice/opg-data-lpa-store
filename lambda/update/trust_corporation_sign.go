ad12910f142fa0d1bf1e0d1d363c27e5902984c9
Joshua Hawxwell <m@hawx.me>
2025-02-28 10:58:53 +0000

    Refactor validation so it works better for both use cases (#300)

--- lambda/update/trust_corporation_sign.go
+++ lambda/update/trust_corporation_sign.go
@@ -51,13 +51,9 @@ func validateTrustCorporationSign(changes []shared.Change, lpa *shared.Lpa) (Tru
 
 					return each.
 						Field("/mobile", &data.Mobile).
-						Field("/contactLanguagePreference", &data.ContactLanguagePreference, parse.Validate(func() []shared.FieldError {
-							return validate.IsValid("", data.ContactLanguagePreference)
-						})).
+						Field("/contactLanguagePreference", &data.ContactLanguagePreference, parse.Validate(validate.Valid())).
 						Field("/email", &data.Email, parse.Optional()).
-						Field("/channel", &data.Channel, parse.Validate(func() []shared.FieldError {
-							return validate.IsValid("", data.Channel)
-						}), parse.Optional()).
+						Field("/channel", &data.Channel, parse.Validate(validate.Valid()), parse.Optional()).
 						Prefix("/signatories", func(prefix *parse.Parser) []shared.FieldError {
 							return prefix.
 								Each(func(i int, each *parse.Parser) []shared.FieldError {
