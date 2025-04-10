807e9d32e9e93516777c9cf4668c982f663c1ad9
Alex Saunders <acsauk@gmail.com>
2025-04-10 11:51:55 +0100

    handle PAPER_CERTIFICATE_PROVIDER_ACCESS_ONLINE updates (#322)

---
+++ lambda/update/paper_certificate_provider_access_online.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/ministryofjustice/opg-data-lpa-store/internal/shared"
+	"github.com/ministryofjustice/opg-data-lpa-store/internal/validate"
+	"github.com/ministryofjustice/opg-data-lpa-store/lambda/update/parse"
+)
+
+type PaperCertificateProviderAccessOnline struct {
+	Email string
+}
+
+func (c PaperCertificateProviderAccessOnline) Apply(lpa *shared.Lpa) []shared.FieldError {
+	if lpa.CertificateProvider.Channel != shared.ChannelPaper {
+		return []shared.FieldError{{Source: "/channel", Detail: "lpa channel is not paper"}}
+	}
+
+	lpa.CertificateProvider.Email = c.Email
+
+	return nil
+}
+
+func validatePaperCertificateProviderAccessOnline(changes []shared.Change, lpa *shared.Lpa) (Applyable, []shared.FieldError) {
+	var data PaperCertificateProviderAccessOnline
+	errors := parse.Changes(changes).
+		Field("/certificateProvider/email", &data.Email, parse.Validate(validate.NotEmpty())).
+		Consumed()
+
+	return data, errors
+}
