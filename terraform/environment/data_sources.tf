6673d7bbee532032ca463159978b2d1c94f7c854
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-04-17 15:17:24 +0100

    MLPBA-2980 - create eventbridge rule for metrics (#327)
    * create eventbridge rule for metrics
    
    * fix role name
    
    * fix role attributes
    
    * send metric events to opg-metrics only

--- terraform/environment/data_sources.tf
+++ terraform/environment/data_sources.tf
@@ -1,3 +1,11 @@
 data "aws_caller_identity" "current" {
   provider = aws.eu_west_1
 }
+
+data "aws_region" "current" {
+  provider = aws.eu_west_1
+}
+
+data "aws_default_tags" "current" {
+  provider = aws.eu_west_1
+}
