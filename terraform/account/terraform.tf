5c29a1de89b899c5975afd29fff0a607a26a9eb9
Josh Murray <jhm1999@hotmail.co.uk>
2026-01-22 15:24:50 +0000

    SP-3412 - OIDC read only account level workflow #minor

--- terraform/account/terraform.tf
+++ terraform/account/terraform.tf
@@ -5,7 +5,7 @@ terraform {
     encrypt = true
     region  = "eu-west-1"
     assume_role = {
-      role_arn = "arn:aws:iam::311462405659:role/lpa-store-ci"
+      role_arn = "arn:aws:iam::311462405659:role/lpa-store-management-ci"
     }
     dynamodb_table = "remote_lock"
   }
