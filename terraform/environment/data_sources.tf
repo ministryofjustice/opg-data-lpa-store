270d72261dc627163719b6ae0ed3336e76b921c1
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-05-14 12:57:21 +0100

    comment out opg metrics resources

--- terraform/environment/data_sources.tf
+++ terraform/environment/data_sources.tf
@@ -2,9 +2,9 @@ data "aws_caller_identity" "current" {
   provider = aws.eu_west_1
 }
 
-data "aws_region" "current" {
-  provider = aws.eu_west_1
-}
+# data "aws_region" "current" {
+#   provider = aws.eu_west_1
+# }
 
 data "aws_default_tags" "current" {
   provider = aws.eu_west_1
